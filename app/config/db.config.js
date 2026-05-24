8c9c7b800e5a13d8da2e950449c015514822acb0
Arianna Chávez Rodríguez <arianna.chavez@tecsup.edu.pe>
2026-05-23 19:06:32 -0500

    Desactivando conexion local de base de datos

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,14 @@
-export default{
-    HOST: "localhost",
-    USER:"root",
-    PASSWORD: "",
-    DB: "db",
-    PORT: 3306,
-    dialect: "mysql",
-    pool:{
-        mas: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-};
+//export default{
+  //  HOST: "localhost",
+    //USER:"root",
+    //PASSWORD: "",
+    //DB: "db",
+    //PORT: 3306,
+    //dialect: "mysql",
+    //pool:{
+       // mas: 5,
+     //   min: 0,
+        //acquire: 30000,
+        //idle: 10000,
+    //},
+//};
